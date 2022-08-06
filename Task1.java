613bdf42fbe65ec1094802c3a45062bd5a9bb3a1
Nataliia Malinitska <nataliiamalinitska@Air-Nataliia.home>
2022-08-06 12:42:31 +0200

    my commit

--- Task1.java
+++
@@ -1,6 +0,0 @@
-public class Task1 {
-    public static void main(String[] args) {
-        String a = "Ukraine!";
-        System.out.println(a);
-    }}
-
--- Task6.java
+++
@@ -1,6 +0,0 @@
-public class Task6 { public static void main(String[] args)
-
-{final int CONSTANTA = 220;
-//        CONSTANTA = 3;
-        System.out.println(CONSTANTA);
-                }}
