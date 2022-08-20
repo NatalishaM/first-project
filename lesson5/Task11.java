5a9c92f9a78dc6d0d2b4bae85f89d07dd3a60d95
nataliiamalinitska <malinitska.natalia@gmail.com>
2022-08-20 14:33:48 +0200

    lesson 4 part 2

---
+++ lesson5/Task11.java
@@ -0,0 +1,19 @@
+package lesson5;
+ //What is the difference between package-private access and protected access?
+
+
+public class Task11 {
+    public static void main(String[] args) {
+        Mama k = new Mama();
+        System.out.println(k.l);
+        k.soul();
+    }
+}
+class Papa {
+    protected String l= "semya";
+    protected void soul(){
+        System.out.println(this.l);
+    }
+}
+class Mama extends Papa {}
+
